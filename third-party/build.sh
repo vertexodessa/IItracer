ac2849faaa4f3d4d751210fe937c0502e1076ea4
iivlev <iivlev@productengine.com>
2017-02-04 19:26:25 +0200

    Few sanity checks in WTF build script

--- third-party/build.sh
+++ third-party/build.sh
@@ -2,18 +2,22 @@
 
 set -ex
 
-pushd .
-cd ./tracing-framework/bindings/cpp/
-# as of 2017/02 default threading won't work; using STD threading model instead
-make THREADING=std
-echo "Going to install Google Tracing Framework"
-sudo make install
-popd
+if [ ! -e /usr/local/lib/libwtf.so ]; then
+    pushd .
+    cd ./tracing-framework/bindings/cpp/
+    # as of 2017/02 default threading won't work; using STD threading model instead
+    make THREADING=std
+    echo "Going to install Google Tracing Framework"
+    sudo make install
+    popd
+fi
 
-# build js stuff
-pushd .
-cd tracing-framework
-npm install
-third_party/anvil-build/setup-local.sh
-third_party/anvil-build/anvil-local.sh deploy -o build-bin/ :wtf_node_js_compiled
-popd
+if [ ! -e third-party/tracing-framework/build-out/wtf_node_js_compiled.js ]; then
+    # build js stuff
+    pushd .
+    cd tracing-framework
+    npm install
+    third_party/anvil-build/setup-local.sh
+    third_party/anvil-build/anvil-local.sh deploy -o build-bin/ :wtf_node_js_compiled
+    popd
+fi
