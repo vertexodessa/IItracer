bba2e4c7afba43682b8598581fd6f68875165a15
iivlev <iivlev@productengine.com>
2017-02-04 19:22:39 +0200

    Adding build steps for JS instrumentation

--- third-party/build.sh
+++ third-party/build.sh
@@ -2,8 +2,18 @@
 
 set -ex
 
+pushd .
 cd ./tracing-framework/bindings/cpp/
 # as of 2017/02 default threading won't work; using STD threading model instead
 make THREADING=std
 echo "Going to install Google Tracing Framework"
 sudo make install
+popd
+
+# build js stuff
+pushd .
+cd tracing-framework
+npm install
+third_party/anvil-build/setup-local.sh
+third_party/anvil-build/anvil-local.sh deploy -o build-bin/ :wtf_node_js_compiled
+popd
